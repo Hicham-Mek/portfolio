fee85e14ce6942c78e51fec3fb944e69d23d3f89
Hicham-Mek <mekidichehichem@gmail.com>
2024-12-24 10:54:18 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+function showHeader() {
+    const show = document.querySelector("header");
+    show.style.left = "0";
+}
+
+
+function hideHeader() {
+    const hide = document.querySelector("header")
+    hide.style.left = "-30rem";
+}
